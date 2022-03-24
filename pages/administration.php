7b6d606db5fcfd96b45ff43857c259f6b3e821d7
Rosta89 <100356938+Rosta89@users.noreply.github.com>
2022-03-24 20:33:42 +0100

    Administrace na vytvoření nové ligy Fixes #2

---
+++ pages/administration.php
@@ -0,0 +1,3 @@
+<div class="text-center mt-5">
+    <a href='index.php?page=competition_create'> Přidat competition </a>
+</div>
---
+++ pages/competition_create.php
@@ -0,0 +1,47 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $comp_create = $comp_err = "";
+    if ($_POST['players'] <= 0) {
+        $comp_err = "vyber počet hráčů nad 0";
+    } else if ($_POST['competition_name'] != "") {
+        if (db::queryAll("SELECT NAME from competitions where NAME = ?", $_POST['competition_name'])) {
+            $comp_err = "competition již existuje";
+        } else {
+            Db::query("INSERT INTO competitions (NAME, PLAYERS_COUNT) VALUES (?,?)", $_POST['competition_name'], $_POST['players']);
+            $comp_create = "vytvořeno";
+        }
+    } else {
+        $comp_err = "nezadal si jméno";
+    }
+}
+
+?>
+
+
+<form action="index.php?page=competition_create" method="post">
+    <?php
+    if (!empty($comp_err)) {
+        echo '<div class="alert alert-danger">' . $comp_err . '</div>';
+    }
+    if (!empty($comp_create)) {
+        echo '<div class="alert alert-success">' . $comp_create . '</div>';
+    }
+    ?>
+    <span>Jméno comptetition :</span>
+    <input type="text" name="competition_name" value="<?php if (isset($_POST['competition_name'])) {
+                                                            echo $_POST['competition_name'];
+                                                        } else {
+                                                            echo '';
+                                                        }  ?>">
+    <span>Počet hráčů :</span>
+    <input type="number" name="players" value="<?php if (isset($_POST['players'])) {
+                                                    echo $_POST['players'];
+                                                } else {
+                                                    echo '0';
+                                                }  ?>">
+
+    <div class=" form-group mt-2">
+        <input type="submit" class="btn btn-primary" value="Vytvořit ligu">
+    </div>
+</form>
+</div>
