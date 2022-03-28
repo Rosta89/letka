48ef50841152cad8a0a90fadf6e36a614713cd98
Rosta89 <100356938+Rosta89@users.noreply.github.com>
2022-03-28 04:06:24 +0200

    Editatace hráčova role

--- pages/team.php
+++ pages/team.php
@@ -1,9 +1,17 @@
 <?php
-
-if (isset($_GET['id']) && Db::querySingle("SELECT COUNT(*) from TEAMS WHERE ID = ?", $_GET['id'])) {
-    $teamID = $_GET['id'];
+$teamID = $_GET['id'];
+if (Db::querySingle("SELECT COUNT(*) from TEAMS WHERE ID = ?", $teamID)) {
+    if (isset($_GET['edit'])) {
+        //ověřit zda má na to právo
+        $playersNumber = Db::querySingle("SELECT COUNT(*) from players_2_teams WHERE TEAM_ID = ?", $teamID);
+        for ($i = 0; $i < $playersNumber; $i++) {
+            $teamRole = $_GET['role'];
+            $playerID = $_GET['playerID'];
+            Db::query('UPDATE players_2_teams SET player_role = ? WHERE PLAYER_ID =? AND TEAM_ID = ?', $teamRole[$i], $playerID[$i], $teamID);
+        }
+    }
     $teamName = Db::querySingle("SELECT NAME from TEAMS WHERE ID = ?", $teamID);
-    $players = Db::queryAll("SELECT pl.name,plt.player_role
+    $players = Db::queryAll("SELECT pl.id,pl.name,plt.player_role
     FROM players pl 
     JOIN players_2_teams plt on plt.player_id = pl.id
     WHERE plt.team_id = ?", $teamID);
@@ -26,45 +34,60 @@ if (isset($_GET['id']) && Db::querySingle("SELECT COUNT(*) from TEAMS WHERE ID =
                         ?>
                     </div>
                     <div class="text-left col-md-6 " style="margin:auto;">
-                        <table class="content-table">
-                            <h2><?= $teamName ?></h2>
-                            <thead>
-                                <tr>
-                                    <th>Jméno</th>
-                                    <th>Role</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($players as $player) {
-                                ?>
+                        <form action="index.php" method="GET">
+                            <table class="content-table">
+                                <input type="hidden" name="page" value="team">
+                                <input type="hidden" name="id" value="<?php echo $teamID; ?>">
+                                <input type="hidden" name="edit" value="true">
+                                <h2><?= $teamName ?></h2>
+                                <thead>
                                     <tr>
-                                        <td><?= $player['name'] ?></td>
-                                        <td>
-                                            <?php
-
-                                            if ($player['player_role'] == 0) {
-                                                echo "Hráč";
-                                            } else if ($player['player_role'] == 1) {
-                                                echo "Kapitán";
-                                            } else {
-                                                echo "Neurčeno";
-                                            }
-                                            ?>
-                                        </td>
+                                        <th>Jméno</th>
+                                        <th>Role</th>
+                                        <th>Edit</th>
                                     </tr>
-                                <?php
-                                }
-                                ?>
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($players as $player) {
+                                    ?>
+                                        <tr>
+                                            <td><?= $player['name'] ?></td>
+                                            <td>
+                                                <?php
+
+                                                if ($player['player_role'] == 0) {
+                                                    echo "Hráč";
+                                                } else if ($player['player_role'] == 1) {
+                                                    echo "Kapitán";
+                                                } else {
+                                                    echo "Neurčeno";
+                                                }
+                                                ?>
+                                            </td>
+                                            <td>
+                                                <input type="hidden" name="playerID[]" value="<?php echo $player['id']; ?>">
+                                                <select name='role[]'>
+                                                    <option value="0">Hráč</option>
+                                                    <option value="1">Kapitán</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                            <input type="submit" class="btn btn-primary" value="Edit">
+                        </form>
                     </div>
+
                 </div>
                 <div class="text-center">
                     <?php
                     if ($leagues) {
                         echo ('<p>Ligy</p>');
-                        foreach ($leagues as $league) {
-                            echo ('<p><a href="index.php?page=table&id=' . $league['COMPETITION_ANNUAL_ID'] . '">' . $league['cName'] . ' ' . $league['caName'] . '</p>');
+                        foreach ($leagues as $leagues) {
+                            echo ('<p><a href="index.php?page=table&id=' . $leagues['COMPETITION_ANNUAL_ID'] . '">' . $leagues['cName'] . ' ' . $leagues['caName'] . '</p>');
                         }
                     } else {
                         echo ('<p>Není v lize</p>');
